eb05df203ef976872d046d8f0e2785b7eda79497
Pablo Polanco <>
2022-07-16 03:49:34 +0000

    Botonos de aumentar y disminuir

---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+    $('#TablaClientes').DataTable();
+});
+
+$("#addcliente-form").submit(async function(form) {
+    form.preventDefault();
+
+    const Nombre = $("#nombreInput").val();
+    const Direccion = $("#direccionInput").val();
+    const Telefono = $("#telefonoInput").val();
+    const Edad = $("#edadInput").val();
+
+    $("#table_body").prepend(
+      ` <tr>
+          <td>${Nombre}</td>
+          <td>${Direccion}</td>
+          <td>${Telefono}</td>
+          <td>${Edad}</td>
+        </tr>`
+    );
+});
+
+$(document).ready(function () {
+    $('#TablaProductos').DataTable();
+});
+
+$("#addproduct-form").submit(async function(form) {
+    form.preventDefault();
+
+    
+    const nombre = $("#nombreProducto").val();
+    const categoria = $("#categoriaProducto").val();
+    const precio = $("#precioProducto").val();
+    const cantidad = $("#cantidadProducto").val();
+
+    $("#producto_table_body").prepend(
+      ` <tr>
+          <td>${nombre}</td>
+          <td>${categoria}</td>
+          <td>${precio}</td>
+          <td>${cantidad}</td>
+          <td class="contador"><button class="btn btn-sm btn-success" >+1</button> <button class="btn btn-sm btn-danger" >-1</button></td>
+        </tr>` 
+    );
+    document.querySelectorAll(".contador").forEach((e) =>{
+        e.firstChild.addEventListener('click', () => {
+            let cont = +e.previousElementSibling.textContent;
+            e.previousElementSibling.innerHTML = cont+1;
+            if (cont+1 >= 20) {
+                e.previousElementSibling.style.color ="green";
+            } else if (cont+1 >= 0) {
+                e.previousElementSibling.style.color ="back";
+            }
+        })
+        e.lastChild.addEventListener('click', () => {
+            let cont = +e.previousElementSibling.textContent;
+            e.previousElementSibling.innerHTML = cont-1;
+            if (cont-1 < 0) {
+                e.previousElementSibling.style.color ="red";
+            } else if (cont-1 >= 0 && cont-1 < 20) {
+                e.previousElementSibling.style.color ="balck";
+            }
+        })
+    })
+});
+
+
+//Funcion del contador
+
+$("#aumenta").click(function () {
+    let cont = +$('#contador').text();
+    document.getElementById("contador").innerHTML = cont+1;
+    if (cont+1 >= 20) {
+        $('#contador').css('color','green');
+    } else if (cont+1 >= 0) {
+        $('#contador').css('color','black');
+    }
+}) 
+$("#disminuye").click(function() {
+    let cont = +$('#contador').text();
+    document.getElementById("contador").innerHTML = cont-1;
+    if (cont-1 < 0) {
+        $('#contador').css('color','red');
+    } else if (cont-1 >= 0 && cont-1 < 20) {
+        $('#contador').css('color','black');
+    }
+    
+})
