7f168528b00da1c5ea991a343d40caf97ad4b6f2
Pablo Polanco Valdes <51938148+Pabpol@users.noreply.github.com>
2022-07-26 03:29:52 +0000

    Funcionalidad agregar al carrito y filtro por nombre y id

--- assets/js/ventas.js
+++ assets/js/ventas.js
@@ -1,128 +1,9 @@
 $(document).ready(function() {
-  $('#TablaClientes').DataTable({
-    scrollX: true
-  });
-});
-
-
-$("#addcliente-form").submit(async function(form) {
-  form.preventDefault();
-
-  const Nombre = $("#nombreInput").val();
-  const Direccion = $("#direccionInput").val();
-  const Telefono = $("#telefonoInput").val();
-  const Edad = $("#edadInput").val();
-
-  $("#table_body").prepend(
-    ` <tr>
-          <td>${Nombre}</td>
-          <td>${Direccion}</td>
-          <td>${Telefono}</td>
-          <td>${Edad}</td>
-        </tr>`
-  );
-});
-
-$(document).ready(function() {
   $('#TablaProductos').DataTable({
     scrollX: true
   });
 });
 
-$("#addproduct-form").submit(async function(form) {
-  form.preventDefault();
-
-
-  const nombre = $("#nombreProducto").val();
-  const categoria = $("#categoriaProducto").val();
-  const precio = $("#precioProducto").val();
-  const cantidad = $("#cantidadProducto").val();
-
-  $("#producto_table_body").prepend(
-    ` <tr>
-          <td>${nombre}</td>
-          <td>${categoria}</td>
-          <td>${precio}</td>
-          <td>${cantidad}</td>
-          <td class="contador"><button class="btn btn-sm btn-success" >+1</button> <button class="btn btn-sm btn-danger" >-1</button></td>
-        </tr>`
-  );
-  document.querySelectorAll(".contador").forEach((e) => {
-    e.firstChild.addEventListener('click', () => {
-      let cont = +e.previousElementSibling.textContent;
-      e.previousElementSibling.innerHTML = cont + 1;
-      if (cont + 1 >= 20) {
-        e.previousElementSibling.style.color = "green";
-      } else if (cont + 1 >= 0) {
-        e.previousElementSibling.style.color = "back";
-      }
-    })
-    e.lastChild.addEventListener('click', () => {
-      let cont = +e.previousElementSibling.textContent;
-      e.previousElementSibling.innerHTML = cont - 1;
-      if (cont - 1 < 0) {
-        e.previousElementSibling.style.color = "red";
-      } else if (cont - 1 >= 0 && cont - 1 < 20) {
-        e.previousElementSibling.style.color = "balck";
-      }
-    })
-  })
-});
-
-//Funcion recibe cliente
-function cliente() {
-  let nombre = prompt('Ingrese su nombre:');
-  let apellido = prompt('Ingrese su apellido:');
-
-  if (nombre === null || apellido === null) {
-    document.getElementById('Nombre_cliente').innerHTML = 'Bienvenido/a';
-  } else {
-    document.getElementById('Nombre_cliente').innerHTML = 'Bienvenido/a ' + nombre + ' ' + apellido;
-  }
-
-}
-
-//Funcion del contador
-
-$("#aumenta").click(function() {
-  let cont = +$('#contador').text();
-  document.getElementById("contador").innerHTML = cont + 1;
-  if (cont + 1 >= 20) {
-    $('#contador').css('color', 'green');
-  } else if (cont + 1 >= 0) {
-    $('#contador').css('color', 'black');
-  }
-})
-$("#disminuye").click(function() {
-  let cont = +$('#contador').text();
-  document.getElementById("contador").innerHTML = cont - 1;
-  if (cont - 1 < 0) {
-    $('#contador').css('color', 'red');
-  } else if (cont - 1 >= 0 && cont - 1 < 20) {
-    $('#contador').css('color', 'black');
-  }
-
-})
-
-$("#aumenta2").click(function() {
-  let cont = +$('#contador2').text();
-  document.getElementById("contador2").innerHTML = cont + 1;
-  if (cont + 1 >= 20) {
-    $('#contador2').css('color', 'green');
-  } else if (cont + 1 >= 0) {
-    $('#contador2').css('color', 'black');
-  }
-})
-$("#disminuye2").click(function() {
-  let cont = +$('#contador2').text();
-  document.getElementById("contador2").innerHTML = cont - 1;
-  if (cont - 1 < 0) {
-    $('#contador2').css('color', 'red');
-  } else if (cont - 1 >= 0 && cont - 1 < 20) {
-    $('#contador2').css('color', 'black');
-  }
-
-})
 // Objetos de productos
 
 function Producto(id, nombre, precio, img) {
@@ -132,24 +13,26 @@ function Producto(id, nombre, precio, img) {
   this.img = img;
 }
 
-function clpFormat(val){
+function clpFormat(val) {
   return new Intl.NumberFormat('es-CL', { currency: 'CLP', style: 'currency' }).format(val)
 }
+const producto1 = new Producto(1, "Telescopio Celestron PowerSeeker 80EQ", 125000, "https://www.telescopioschile.cl/wp-content/uploads/2020/07/Telescopio-Celestron-PowerSeeker-80EQ.jpg");
+const producto2 = new Producto(2, "Reloj", 300000, "https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80");
+const producto3 = new Producto(3, "Audífonos", 100000, "https://images.unsplash.com/photo-1583394838336-acd977736f90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=684&q=80");
+const producto4 = new Producto(4, "Audífonos", 200000, "https://images.unsplash.com/photo-1606741965509-717b9fdd6549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80");
+const producto5 = new Producto(5, "Silla", 400000, "https://img.freepik.com/psd-gratis/vista-frontal-maqueta-silla-juegos_1332-21844.jpg?w=740&t=st=1658366671~exp=1658367271~hmac=9ff01b06cf7d695c282efd19a22fe049899865dedf6e3558c2510b9cd3257eb6");
+const producto6 = new Producto(6, "Notebook", 450000, "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGxhcHRvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60");
 
-const producto1 = new Producto(1, "Reloj", 300000, "https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80");
-const producto2 = new Producto(2, "Audífonos", 100000, "https://images.unsplash.com/photo-1583394838336-acd977736f90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=684&q=80");
-const producto3 = new Producto(3, "Audífonos", 200000, "https://images.unsplash.com/photo-1606741965509-717b9fdd6549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80");
-const producto4 = new Producto(4, "Silla", 400000, "https://img.freepik.com/psd-gratis/vista-frontal-maqueta-silla-juegos_1332-21844.jpg?w=740&t=st=1658366671~exp=1658367271~hmac=9ff01b06cf7d695c282efd19a22fe049899865dedf6e3558c2510b9cd3257eb6");
-
-const productos = [producto1, producto2, producto3, producto4];
+const productos = [producto1, producto2, producto3, producto4, producto5, producto6];
 const listadoProductos = document.getElementById('listadoProductos');
+
 productos.forEach(function(producto) {
   listadoProductos.innerHTML +=
-    `<div class="col-sm-4 card d-flex align-items-stretch">
+    `<div class="col-sm-4 card align-items-stretch">
           <img class="card-img-top img-thumbnail mt-2 product-img" src="${producto.img}">
             
           <div class="card-body d-flex flex-column">
-            <h5 class="card-title">${producto.nombre}</h5>
+            <h5 class="card-title">${producto.nombre}</h5> <span><p>SKU: ${producto.id}</p></span>
             
             <h5 class="card-footer mt-auto"> ${clpFormat(producto.precio)}</h5>
             
@@ -166,7 +49,81 @@ productos.forEach(function(producto) {
               <label for="sel1" class="form-label">Cantidad:</label>
             </div>
             <br>
-            <a href="#" class="btn btn-dark mt-auto align-self-start">Agregar</a>
+            <a class="agregar-carrito btn btn-dark mt-auto align-self-start">Agregar</a>
           </div>
         </div>`
 })
+
+// Filtrar productos
+function FiltroProductos() {
+  var entrada = document.getElementById('filtro').value.toLowerCase();
+  var contenedor = document.getElementById('listadoProductos');
+  var productos = contenedor.getElementsByClassName('card');
+
+  for (let i = 0; i < productos.length; i++) {
+    var nombreProd = productos[i].querySelector('.card-body h5.card-title');
+    var skuProd = productos[i].querySelector('.card-body span p');
+
+    if (nombreProd.innerText.toLowerCase().indexOf(entrada) > -1 || skuProd.innerText.substr(-1).indexOf(entrada) > -1) {
+      productos[i].style.display = '';
+    } else {
+      productos[i].style.display = 'none';
+    }
+  }
+}
+
+// Objetos de productosCarrito
+
+function ProductoCarrito(id, nombre, precio, cantidad) {
+  this.id = id;
+  this.nombre = nombre;
+  this.precio = precio;
+  this.cantidad = cantidad;
+}
+ProductoCarrito.prototype.actualizarCantidad = function(nuevaCantidad) {
+  this.cantidad = nuevaCantidad;
+}
+const productosCarrito = [];
+
+function addCarrito(p) {
+  productosCarrito.push(p);
+}
+
+document.querySelectorAll(".agregar-carrito").forEach((e) => {
+  e.addEventListener('click', () => {
+    let cant = e.parentNode.children[3].children[0].value;
+    let nombre = e.parentNode.children[0].textContent;
+    let precio = e.parentNode.children[2].textContent;
+    let id = e.parentNode.children[1].textContent.split("SKU: ")[1];
+    const prodCarrito = new ProductoCarrito(id, nombre, precio, cant);
+    addCarrito(prodCarrito);
+    actualizarContador(productosCarrito.length);
+  })
+})
+
+//Poblar el carrito
+function poblarCarrito() {
+  const bodyTabla = document.getElementById('body-tabla');
+  productosCarrito.forEach(function(productoCarrito) {
+    bodyTabla.innerHTML +=
+      `<tr>
+        <td>${productoCarrito.nombre}</td>
+        <td>${productoCarrito.precio}</td>
+        <td>${productoCarrito.cantidad}</td>
+        <td>${parseInt(productoCarrito.cantidad) * parseInt(productoCarrito.precio.split("$")[1].replace(".", ""))}</td>
+      </tr>`
+  });
+}
+
+document.getElementById('carrito').addEventListener('click', () => {
+  const bodyTabla = document.getElementById('body-tabla');
+  bodyTabla.innerHTML = "";
+  poblarCarrito();
+})
+
+//Actualizar contador carrito
+function actualizarContador(valor) {
+  const contador = document.getElementById('contadorProductor')
+  contador.innerHTML = `${valor}`
+}
+
